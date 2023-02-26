2dd8ac06c3c379d4bc6910218bec60c1b3a2e8db
taoyunfei <t1290185595@qq.com>
2023-02-26 19:52:06 +0800

    .

--- lab/user/pingpong.c
+++ lab/user/pingpong.c
@@ -1,7 +1,9 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+
 #define MSGSIZE 16
+
 int main (int argc, char *argv[]) {
     int fd[2];
     char buf[MSGSIZE];
@@ -9,9 +11,12 @@ int main (int argc, char *argv[]) {
     int pid = fork();
     if (pid > 0) {
         write(fd[1], "ping", MSGSIZE);
+        wait(0);
+        read(fd[0], buf, MSGSIZE);
     } else {
         read(fd[0], buf, MSGSIZE);
-        printf("%s", buf);
+        write(fd[1], "pong", MSGSIZE);
     }
+    printf("%d: received %s\n", pid, buf);
     exit(0);
 }
