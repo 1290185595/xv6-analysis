451579e5283180ed343b34e306d9e887484cd3aa
1290185595 <t1290185595@qq.com>
2023-03-03 00:50:45 +0800

    .

--- build.bat
+++ build.bat
@@ -11,11 +11,14 @@ if exist %LOCAL_PROJECT% rmdir /s/q %LOCAL_PROJECT%
 rename %PROJECT% %LOCAL_PROJECT%
 
 cd %LOCAL_PROJECT%
+echo %cd%
+git config --global --add safe.directory %cd:\=/%
 git checkout %1%
 git branch -a | findstr "*" > .branch
 rmdir /s/q .git
 del .gitignore
 cd ..
+
 
 pip install -r requirements.txt
 python modify.py --operation=build
