3f760ea2df14425ae9c3707c3de5fd00afc1155b
taoyunfei <t1290185595@qq.com>
2023-03-05 18:54:02 +0800

    .

--- cow/kernel/kalloc.c
+++ cow/kernel/kalloc.c
@@ -1,132 +1,3 @@
-//// Physical memory allocator, for user processes,
-//// kernel stacks, page-table pages,
-//// and pipe buffers. Allocates whole 4096-byte pages.
-//
-//#include "types.h"
-//#include "param.h"
-//#include "memlayout.h"
-//#include "spinlock.h"
-//#include "riscv.h"
-//#include "defs.h"
-//
-//void freerange(void *pa_start, void *pa_end);
-//
-//int pa2index(void *pa);
-//
-//extern char *end; // first address after kernel.
-//// defined by kernel.ld.
-//
-//struct run {
-//    struct run *next;
-//};
-//
-//struct {
-//    struct spinlock lock;
-//    struct run *freelist;
-//    char *pa_ref_cnt;
-//} kmem;
-//
-//void
-//kinit() {
-//    initlock(&kmem.lock, "kmem");
-//
-//    int i = pa2index((void *) PHYSTOP);
-//    acquire(&kmem.lock);
-//    kmem.pa_ref_cnt = end;
-//    end += i;
-//    printf("kmem.pa_ref_cnt:%p, end=%p, i = %d\n", kmem.pa_ref_cnt, end, i);
-//    memset(kmem.pa_ref_cnt, 1, i);
-//    release(&kmem.lock);
-//
-//
-//    freerange(end, (void *) PHYSTOP);
-//}
-//
-//
-//int pa2index(void *pa) {
-//    printf("pa2index %p => %d\n", pa, (int) (((uint64) pa - PGROUNDUP((uint64) end)) >> 12));
-//    return (int) (((uint64) pa - PGROUNDUP((uint64) end)) >> 12);
-//}
-//
-//
-//void
-//freerange(void *pa_start, void *pa_end) {
-//    char *p;
-//    pa_start = (char *) PGROUNDUP((uint64) pa_start);
-//
-//    for (p = pa_start; p + PGSIZE <= (char *) pa_end; p += PGSIZE) {
-//        kfree(p);
-//    }
-//}
-//
-//// Free the page of physical memory pointed at by pa,
-//// which normally should have been returned by a
-//// call to kalloc().  (The exception is when
-//// initializing the allocator; see kinit above.)
-//void
-//kfree(void *pa) {
-//    struct run *r;
-//
-//    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP) {
-//        panic("kfree");
-//    }
-//    if (--kmem.pa_ref_cnt[pa2index(pa)] == 0) {
-//
-//        // Fill with junk to catch dangling refs.
-//        memset(pa, 1, PGSIZE);
-//        r = (struct run *) pa;
-//
-//        acquire(&kmem.lock);
-//        r->next = kmem.freelist;
-//        kmem.freelist = r;
-//        release(&kmem.lock);
-//    }
-//}
-//
-//void
-//kkeep(void *pa) {
-//    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP) {
-//        panic("kkeep");
-//    }
-//
-//    acquire(&kmem.lock);
-//    ++kmem.pa_ref_cnt[pa2index(pa)];
-//    release(&kmem.lock);
-//}
-//
-//// Allocate one 4096-byte page of physical memory.
-//// Returns a pointer that the kernel can use.
-//// Returns 0 if the memory cannot be allocated.
-//void *
-//kalloc(void) {
-//    struct run *r;
-//
-//    acquire(&kmem.lock);
-//    r = kmem.freelist;
-//    if (r) {
-//        kmem.freelist = r->next;
-//        kmem.pa_ref_cnt[pa2index(r)] = 1;
-//    }
-//    release(&kmem.lock);
-//
-//    if (r) {
-//        memset((char *) r, 5, PGSIZE); // fill with junk
-//    }
-//    return (void *) r;
-//}
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////
-//// Physical memory allocator, for user processes,
-//// kernel stacks, page-table pages,
-//// and pipe buffers. Allocates whole 4096-byte pages.
-//
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
@@ -148,58 +19,51 @@ struct run {
 struct {
     struct spinlock lock;
     struct run *freelist;
-    char * end;
-    char * pa_cnt;
+    char *end;
+    char *pa_cnt;
 } kmem;
 
 void
 kinit() {
     kmem.pa_cnt = end;
-    kmem.end = (char *) PGROUNDUP((uint64)(end + pa2idx((void *) PHYSTOP)));
-
     initlock(&kmem.lock, "kmem");
-    freerange(kmem.end, (void *) PHYSTOP);
+    freerange(end + pa2idx((void *) PHYSTOP), (void *) PHYSTOP);
 }
 
 int pa2idx(void *pa) {
     return ((PGROUNDUP((uint64) pa) - PGROUNDUP((uint64) end)) >> 12);
 }
 
-void kkeep(void *) {
-
-}
-
-void
-freerange(void *pa_start, void *pa_end) {
-    char *p;
-    p = (char *) PGROUNDUP((uint64) pa_start);
-    for (; p < (char *) pa_end; p += PGSIZE)
-        kfree(p);
-}
-
-// Free the page of physical memory pointed at by pa,
-// which normally should have been returned by a
-// call to kalloc().  (The exception is when
-// initializing the allocator; see kinit above.)
-void
-_kfree(void *pa) {
-    struct run *r = (struct run *) pa;
-
+void kkeep(void * pa) {
     acquire(&kmem.lock);
-    r->next = kmem.freelist;
-    kmem.freelist = r;
-    kmem.pa_cnt[pa2idx(pa)] = 0;
+    ++kmem.pa_cnt[pa2idx(pa)];
     release(&kmem.lock);
 }
 
-void
-kfree(void *pa) {
-    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP)
-        panic("kfree");
+void freerange(void *pa_start, void *pa_end) {
+    for (char *pa = (char *) PGROUNDUP((uint64) pa_start); pa < (char *) pa_end; pa += PGSIZE) {
+        acquire(&kmem.lock);
+        kmem.pa_cnt[pa2idx(pa)] = 1;
+        kfree(pa);
+        release(&kmem.lock);
+    }
+}
 
-    // Fill with junk to catch dangling refs.
-    memset(pa, 1, PGSIZE);
-    _kfree(pa);
+
+void kfree(void *pa) {
+    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP) {
+        panic("kfree");
+    }
+    struct run *r = (struct run *) pa;
+    acquire(&kmem.lock);
+    if (--kmem.pa_cnt[pa2idx(pa)] == 0) {
+        r->next = kmem.freelist;
+        kmem.freelist = r;
+        release(&kmem.lock);
+        memset(pa, 1, PGSIZE);
+    } else {
+        release(&kmem.lock);
+    }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -211,11 +75,13 @@ kalloc(void) {
 
     acquire(&kmem.lock);
     r = kmem.freelist;
-    if (r)
+    if (r) {
         kmem.freelist = r->next;
+    }
     release(&kmem.lock);
-
-    if (r)
+    kkeep(r);
+    if (r) {
         memset((char *) r, 5, PGSIZE); // fill with junk
+    }
     return (void *) r;
 }
