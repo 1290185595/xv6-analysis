f3677ee4411d692e1f67eeb6e6a8037b345ec995
taoyunfei <t1290185595@qq.com>
2023-03-05 17:37:30 +0800

    .

--- cow/kernel/kalloc.c
+++ cow/kernel/kalloc.c
@@ -146,6 +146,7 @@ struct run {
 struct {
     struct spinlock lock;
     struct run *freelist;
+    char * pa_cnt;
 } kmem;
 
 void
@@ -171,14 +172,8 @@ freerange(void *pa_start, void *pa_end) {
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
-kfree(void *pa) {
+_kfree(void *pa) {
     struct run *r;
-
-    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP)
-        panic("kfree");
-
-    // Fill with junk to catch dangling refs.
-    memset(pa, 1, PGSIZE);
 
     r = (struct run *) pa;
 
@@ -186,6 +181,15 @@ kfree(void *pa) {
     r->next = kmem.freelist;
     kmem.freelist = r;
     release(&kmem.lock);
+}
+void
+kfree(void *pa) {
+    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP)
+        panic("kfree");
+
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
+    _kfree(pa);
 }
 
 // Allocate one 4096-byte page of physical memory.
