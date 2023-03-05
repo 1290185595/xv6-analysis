523acb83a80afdba69e272e5822057173bec2ad7
taoyunfei <t1290185595@qq.com>
2023-03-05 15:49:19 +0800

    .

--- cow/kernel/kalloc.c
+++ cow/kernel/kalloc.c
@@ -12,32 +12,30 @@
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+// defined by kernel.ld.
 
 struct run {
-  struct run *next;
-  int keeper;
+    struct run *next;
+    int keeper;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+    struct spinlock lock;
+    struct run *freelist;
 } kmem;
 
 void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+kinit() {
+    initlock(&kmem.lock, "kmem");
+    freerange(end, (void *) PHYSTOP);
 }
 
 void
-freerange(void *pa_start, void *pa_end)
-{
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+freerange(void *pa_start, void *pa_end) {
+    char *p;
+    p = (char *) PGROUNDUP((uint64) pa_start);
+    for (; p + PGSIZE <= (char *) pa_end; p += PGSIZE)
+        kfree(p);
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -45,15 +43,15 @@ freerange(void *pa_start, void *pa_end)
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
-kfree(void *pa)
-{
-  struct run *r;
+kfree(void *pa) {
+    struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP) {
+        panic("kfree");
+    }
 
-    r = (struct run*)pa;
-    if (--r->keeper==0) {
+    r = (struct run *) pa;
+    if (--r->keeper == 0) {
 
         // Fill with junk to catch dangling refs.
         memset(pa, 1, PGSIZE);
@@ -65,15 +63,16 @@ kfree(void *pa)
         release(&kmem.lock);
     }
 }
+
 void
-kkeep(void *pa)
-{
+kkeep(void *pa) {
     struct run *r;
 
-    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP) {
         panic("kkeep");
+    }
 
-    r = (struct run*)pa;
+    r = (struct run *) pa;
     ++r->keeper;
 }
 
@@ -81,19 +80,18 @@ kkeep(void *pa)
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
-{
-  struct run *r;
+kalloc(void) {
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r = kmem.freelist;
+    if (r)
+        kmem.freelist = r->next;
+    release(&kmem.lock);
 
-  if(r) {
-      memset((char *) r, 5, PGSIZE); // fill with junk
-      r->keeper = 1;
-  }
-  return (void*)r;
+    if (r) {
+        memset((char *) r, 5, PGSIZE); // fill with junk
+        r->keeper = 1;
+    }
+    return (void *) r;
 }
