d1db501ad718ee4203c4f754596c98170cb8c09f
taoyunfei <t1290185595@qq.com>
2023-05-04 16:58:54 +0800

    .

--- util/user/find.c
+++ util/user/find.c
@@ -67,6 +67,7 @@ main(int argc, char *argv[])
     if(argc == 2){
         find(".", argv[1]);
     } else if(argc == 3){
+        printf("%s %s\n", argv[1], argv[2]);
         find(argv[1], argv[2]);
     }
     exit(0);
